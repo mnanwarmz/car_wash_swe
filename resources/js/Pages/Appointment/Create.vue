00f91e209a53f18a6340a3a18a8132e4dea9d4b7
danial <danialarrifin11@gmail.com>
2021-11-10 15:07:38 +0800

    appoitnment create vue

--- resources/js/Pages/Appointment/Create.vue
+++ resources/js/Pages/Appointment/Create.vue
@@ -1,18 +1,122 @@
 <template>
-    <div>Create Works!</div>
+<Navbartest></Navbartest>
+ <p class="text-center text-3xl m-8">Make an Appointment</p>
+<div class="md:container md:mx-auto px-4 bg-green-200 m-8">
+ <div class="hidden sm:block" aria-hidden="true">
+    <div class="py-5">
+      <!-- <p class="text-center text-3xl text-white">Make an Appointment</p> -->
+      <div class="border-t border-gray-200" />
+    </div>
+  </div>
+
+  <div class="mt-12 sm:mt-0 m-8  ">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="mt-5 md:mt-0 md:col-span-2 mb-8">
+        <form action="#" method="POST">
+          <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+              <div class="grid grid-cols-6 gap-6">
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="first-name" class="block text-sm font-medium text-gray-700">Vehicle Registration</label>
+                  <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div>
+
+                 <div class="col-span-6 sm:col-span-3">
+                  <label for="country" class="block text-sm font-medium text-gray-700">Time Slot</label>
+                  <select id="time" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <option>11-1130</option>
+                    <option>12-1230</option>
+                    <option>1-130</option>
+                  </select>
+                </div>
+
+                <!-- <div class="col-span-6 sm:col-span-3">
+                  <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
+                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div> -->
+
+                <!-- <div class="col-span-6 sm:col-span-4">
+                  <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
+                  <input type="text" name="email-address" id="email-address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div> -->
+                   <div class="col-span-6 sm:col-span-4">
+                  <label for="country" class="block text-sm font-medium text-gray-700">Appointment Type</label>
+                  <select id="time" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <option>Vacuum</option>
+                    <option>Polish</option>
+                    <option>Standard</option>
+                  </select>
+                </div>
+
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
+                  <select id="country" name="country" autocomplete="country-name" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>Mexico</option>
+                  </select>
+                </div>
+
+                <!-- <div class="col-span-6">
+                  <label for="street-address" class="block text-sm font-medium text-gray-700">Street address</label>
+                  <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+                  <input type="text" name="city" id="city" autocomplete="address-level2" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label for="region" class="block text-sm font-medium text-gray-700">State / Province</label>
+                  <input type="text" name="region" id="region" autocomplete="address-level1" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div>
+
+                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP / Postal code</label>
+                  <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                </div> -->
+              </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Create Appointment
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    <div>
+     <p class="text-center text-2xl ">Notes</p>
+     <p class="text-center text-base m- ">Appointment Type :-</p>
+     <p class="text-center text-base mt-8">Full car wash (Available only at Branches) : </p>
+     <p class="text-center text-base">Car wash with Vacuum and Polish</p>
+     <p class="text-center text-base mt-8 ">Wash only :</p>
+     <p class="text-center text-base">Car wash without vacuum or polish</p>
+      <p class="text-center text-base mt-8 ">Wash + Vacuum :</p>
+     <p class="text-center text-base">Car wash with vacuuming servics</p>
+    </div>
+    </div>
+  </div>
+</div>
 </template>
 
+
+
 <script>
+import Navbartest from "@/Components/NavbarTest";
 export default {
-    components: {},
-
-    props: {},
-
+    props: ["appointments"],
+    computed: {},
+    components: {
+        Navbartest,
+    },
+    created() {
+        console.log("Created" + this.appointments);
+    },
     data() {
         return {};
     },
-
-    computed: {},
 
     methods() {},
 };
