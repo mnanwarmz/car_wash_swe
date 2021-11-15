67b495f9726e7995136c4a91227980e71fd8b464
Poszz <1181201507@student.mmu.edu.my>
2021-11-15 14:45:28 +0800

    Payment page front end

---
+++ resources/js/Pages/Payment/Payment.vue
@@ -0,0 +1,20 @@
+<template>
+    <app-layout title="Payment page">
+        <h1 class="text-xl"></h1>
+        
+
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import Welcome from '@/Jetstream/Welcome.vue'
+    import Stripe from '@/Jetstream/Stripe.vue'
+    export default defineComponent({
+        components: {
+            AppLayout,
+            Welcome,
+        },
+    })
+</script>
